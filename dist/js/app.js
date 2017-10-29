7f65cbe9d4955f6202d334228cec5a75778b54b0
Sergey Scherbo <sergey31scherbo@gmail.com>
2017-10-30 00:33:15 +0400

    first and very basic version

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,10 +1,25 @@
 'use strict';
 
 var gallery = document.querySelector('.gallery');
+var shadow = document.querySelector('.shadow');
+var imageOuter = document.querySelector('.image');
+var image = document.querySelector('.image img');
 
 gallery.addEventListener('click', function (e) {
   e.preventDefault();
-  var image = e.target;
+  var imagePrev = e.target;
+  var imageAttr = imagePrev.getAttribute('src');
+  console.log(imageAttr);
 
-  console.log(image.getAttribute('src'));
+  image.setAttribute('src', imageAttr);
+
+  shadow.classList.add('show');
+  imageOuter.classList.add('show');
+
+  shadow.addEventListener('click', function (e) {
+    if (e.target.classList.contains('shadow')) {
+      e.target.classList.remove('show');
+      imageOuter.classList.remove('show');
+    }
+  });
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,25 @@
-const gallery = document.querySelector('.gallery');
+const gallery    = document.querySelector('.gallery');
+const shadow     = document.querySelector('.shadow');
+const imageOuter = document.querySelector('.image');
+const image      = document.querySelector('.image img');
 
 
 gallery.addEventListener('click', function(e) {
   e.preventDefault();
-  const image = e.target;
+  const imagePrev = e.target;
+  const imageAttr = imagePrev.getAttribute('src');
+  console.log(imageAttr);
 
-  console.log(image.getAttribute('src'));
+  image.setAttribute('src', imageAttr);
+
+
+  shadow.classList.add('show');
+  imageOuter.classList.add('show');
+
+  shadow.addEventListener('click', function(e) {
+    if (e.target.classList.contains('shadow')) {
+      e.target.classList.remove('show');
+      imageOuter.classList.remove('show');
+    }
+  })
 });
